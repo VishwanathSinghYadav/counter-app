eb212a75bb9668743b9e4c82ba3245e6cc20b063
VishwanathSinghYadav <singhvishwanathyadav@gmail.com>
2022-01-28 19:19:09 +0530

    added save count and reset button

--- index.js
+++ index.js
@@ -3,5 +3,20 @@ let countEl = document.getElementById("count-id");
 
 function increment() {
     count += 1;
-    countEl.innerText = count
+    countEl.innerHTML = count;
+};
+
+let saveCountVar = [];
+let saveEl = document.getElementById("save-id");
+
+function saveCount() {
+    saveCountVar.push(count);
+    saveEl.innerHTML = saveCountVar;
+};
+
+function reset() {
+    count = 0;
+    saveCountVar = [];
+    countEl.innerHTML = count;
+    saveEl.innerHTML = saveCountVar;
 }
